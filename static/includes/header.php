ed52cd9a458e8297a965c4334210ad2f751f34a3
viktor <viktrgulakv@rambler.ru>
2017-05-22 11:23:39 +0300

    upd:страница карты

--- static/includes/header.php
+++ static/includes/header.php
@@ -3,7 +3,6 @@
 <head>
 	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>Сайт</title>
 	<link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i,800,800i&amp;subset=cyrillic" rel="stylesheet">
 
@@ -12,7 +11,7 @@
 	<script src="/static/theme/js/libs.js"></script>
 	<script src="/static/theme/js/main.js"></script>
 </head>
-<body>
+<body >
 <header class="header">
 	<div class="header__top">
 		<div class="grid__inner">
--- static/index-tablet.php
+++ static/index-tablet.php
@@ -1,4 +1,74 @@
-<?include "includes/header.php"?>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+	<meta charset="utf-8">
+	<title>Сайт</title>
+	<link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i,800,800i&amp;subset=cyrillic" rel="stylesheet">
+
+	<link rel="stylesheet" href="/static/theme/css/styles.css">
+
+	<script src="/static/theme/js/libs.js"></script>
+	<script src="/static/theme/js/main.js"></script>
+</head>
+<body class="tablet-v">
+<header class="header">
+	<div class="header__top">
+		<div class="grid__inner">
+			<div class="header__inner">
+				<div class="top-menu">
+					<div class="top-menu__item">
+						<a href="#" class="top-menu__link _arr">Частным лицам</a>
+					</div>
+					<div class="top-menu__item">
+						<a href="#" class="top-menu__link _arr">Москва</a>
+					</div>
+					<div class="top-menu__item">
+						<a href="#" class="top-menu__link">Офисы и банкоматы</a>
+					</div>
+					<div class="top-menu__item">
+						<a href="#" class="top-menu__link">Контакты</a>
+					</div>
+					<div class="top-menu__item">
+						<a href="#" class="top-menu__link">Поддержка</a>
+					</div>
+					<div class="top-menu__item _login">
+						<a href="#" class="top-menu__link">Интернет-банк</a>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="header__main">
+		<div class="grid__inner">
+			<div class="header__inner">
+				<a href="#" class="header__logo"></a>
+				<div class="header__menu">
+					<div class="header__menu-item">
+						<a href="#" class="header__menu-link _active">Карты</a>
+					</div>
+					<div class="header__menu-item">
+						<a href="#" class="header__menu-link">Вклады</a>
+					</div>
+					<div class="header__menu-item">
+						<a href="#" class="header__menu-link">Кредиты</a>
+					</div>
+					<div class="header__menu-item">
+						<a href="#" class="header__menu-link">Сберегательный счет</a>
+					</div>
+					<div class="header__menu-item">
+						<a href="#" class="header__menu-link">Платежи</a>
+					</div>
+					<div class="header__menu-item">
+						<a href="#" class="header__menu-link">Еще <span class="dots">...</span></a>
+					</div>
+				</div>
+				<a href="#" class="header__main-btn btn">Онлайн-заявка</a>
+			</div>
+		</div>
+	</div>
+</header>
+<div class="grid">
 	<a href="#" class="next-page"></a>
 	<div class="grid__inner">
 		<div class="card">
