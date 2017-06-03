dd8efced54ecb5d9dd52c8ed2f47f3a181b59c71
viktor <viktrgulakv@rambler.ru>
2017-06-03 19:41:10 +0300

    fix:заявка

--- static/views/request.php
+++ static/views/request.php
@@ -1,39 +1,42 @@
-<div class="b-block <?=$parameters['type'] == 'close' ? '  _transparent request-form__close':'' ?>"  data-constructor-content="request">
-	<a href="javascript:void(0);" class="b-block__edit" data-block-edit-btn="request"></a>
-	<div class="b-block__title h3 <?=$parameters['type'] == 'open' ? '  _dash':'' ?>">Заголовок</div>
-	<div class="b-block__subtitle">Подстрочник.</div>
-<?if($parameters['type'] == 'close' && $parameters['closeDecor'] && $parameters['closeDecor']!='none'){?>
-		<div class="request-form__img"></div>
-<?}?>
-	<div class="request-form">
-<?if($parameters['type'] == 'open' && $parameters['openDecor'] && $parameters['openDecor']!='none'){?>
-		<div class="request-form__img"></div>
-<?}?>
-		<div class="request-form__block b-form">
-			<form action="">
-<?if($parameters['type'] == 'open'){for ($i = 0; $i < $parameters['fieldsCounter']; $i++) { ?>
-				<div class="b-form__row">
-					<div class="b-form__block">
-						<div class="b-form__block-field">
-							<input type="text" placeholder="Placeholder" >
+<div data-constructor-content="request" class="request-form__indent">
+	<div class="b-block <?=$parameters['type'] == 'close' ? '  _transparent request-form__close':'' ?>"  >
+		<a href="javascript:void(0);" class="b-block__edit" data-block-edit-btn="request"></a>
+		<div class="b-block__title h3 <?=$parameters['type'] == 'open' ? '  _dash':'' ?>">Заголовок</div>
+		<div class="b-block__subtitle">Подстрочник.</div>
+		<?if($parameters['type'] == 'close' && $parameters['closeDecor'] && $parameters['closeDecor']!='none'){?>
+			<div class="request-form__img"></div>
+		<?}?>
+		<div class="request-form">
+			<?if($parameters['type'] == 'open' && $parameters['openDecor'] && $parameters['openDecor']!='none'){?>
+				<div class="request-form__img"></div>
+			<?}?>
+			<div class="request-form__block b-form">
+				<form action="">
+					<?if($parameters['type'] == 'open'){for ($i = 0; $i < $parameters['fieldsCounter']; $i++) { ?>
+						<div class="b-form__row">
+							<div class="b-form__block">
+								<div class="b-form__block-field">
+									<input type="text" placeholder="Placeholder" >
+								</div>
+							</div>
 						</div>
-					</div>
-				</div>
-<?}if ($parameters['check']){?>
-				<div class="b-form__row">
-					<div class="b-form__block">
-						<div class="b-check b-form__check">
-							<label><input type="checkbox" checked > Checkbox</label>
+					<?}if ($parameters['check']){?>
+						<div class="b-form__row">
+							<div class="b-form__block">
+								<div class="b-check b-form__check">
+									<label><input type="checkbox" checked > Checkbox</label>
+								</div>
+							</div>
 						</div>
+					<?}}?>
+					<div class="b-form__btn-wrap">
+						<button type="submit" class="btn">Оформить карту</button>
+						<a href="#" class="btn _transparent">Заказать звонок</a>
 					</div>
-				</div>
-<?}}?>
-				<div class="b-form__btn-wrap">
-					<button type="submit" class="btn">Оформить карту</button>
-					<a href="#" class="btn _transparent">Заказать звонок</a>
-				</div>
-			</form>
+				</form>
+			</div>
 		</div>
 	</div>
 </div>
+
 
