d3c9d59c311e4a08fabebe9f4b3913aa29b6d8ed
viktor <viktrgulakv@rambler.ru>
2017-06-14 18:53:30 +0300

    fix:правки

--- static/views/instruction.php
+++ static/views/instruction.php
@@ -6,13 +6,19 @@
 			<span class="b-block__edit-text-in">Редактировать</span>
 		</span>
 	</a>
-	<div class="b-block__title h3 _dash">Как получить карту?</div>
+	<div class="instruction__top">
+		<div class="b-block__title h3 _dash">Как получить карту?</div>
+		<div class="tabs instruction__tabs">
+			<a href="#" class="tabs__item _active">Неименную</a>
+			<a href="#" class="tabs__item">Именную</a>
+		</div>
+	</div>
 <?if ($parameters['stepsCounter']){?>
 	<div class="b-steps _indent <?=$parameters['decor'] && $parameters['decor']!='none'?' _arrs':''?> _<?=$parameters['stepsCounter'];?>-steps">
 <? for ($i = 0; $i < $parameters['stepsCounter']; $i++) { ?>
 		<div class="b-steps__item">
 <?if($parameters['decor'] && $parameters['decor']!='none'){?>
-			<div class="b-steps__item-ico" style="background-image: url('/static/theme/images/icons/get-card/<?=$i%3+1?>.svg')"></div>
+			<div class="b-steps__item-ico" style="background-image: url('/static/theme/images/icons/instruction/<?=$i%3+1?>.svg')"></div>
 <?}?>
 			<div class="b-steps__item-title"><?='1. Шаг'?></div>
 			<div class="b-steps__item-text">Описание действий<br/>для шага в две строки.</div>
