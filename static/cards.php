5fd65e732e641abf9df6c46837b3107081bd45e8
viktor <viktrgulakv@rambler.ru>
2017-06-28 18:50:09 +0300

    add:разводящая карт

---
+++ static/cards.php
@@ -0,0 +1,92 @@
+<?include "includes/header.php"?>
+	<div class="banner _credits" style="background-image: url('/static/theme/images/banners/credits.png');">
+		<div class="grid__inner">
+			<div class="banner__inner">
+					<div class="banner__title">Карты</div>
+			</div>
+		</div>
+	</div>
+	<div class="page-menu__wrap" data-page-menu>
+		<div class="page-menu__holder"></div>
+		<div class="page-menu">
+			<div class="grid__inner">
+				<div class="page-menu__in">
+					<div class="tabs page-menu__items">
+						<a href="javascript:void(0);" class="tabs__item _active">Кредитные карты</a>
+						<a href="javascript:void(0);" class="tabs__item">Дебетовые карты</a>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="cards">
+		<div class="grid__inner">
+			<div class="b-block cards__item">
+				<div class="cards__item-pattern"></div>
+				<div class="cards__item-img" style="background-image: url('/static/theme/images/cards/1.png');"></div>
+				<div class="b-block__title h3">
+					<a href="index.php">Элемент 120</a>
+				</div>
+				<div class="b-block__subtitle">Платежная карта для покупок.</div>
+				<div class="card__options">
+					<div class="card__option">
+						<div class="card__option-val">500 000 ₽</div>
+						<div class="card__option-title">
+							Кредитный лимит
+						</div>
+					</div>
+					<div class="card__option">
+						<div class="card__option-val">120 дней</div>
+						<div class="card__option-title">
+							Без процентов
+						</div>
+					</div>
+				</div>
+				<div class="card__btn"><a href="anketa-card.php" class="btn">Оформить карту</a></div>
+			</div>
+			<div class="b-block cards__item">
+				<div class="cards__item-pattern"></div>
+				<div class="cards__item-img" style="background-image: url('/static/theme/images/cards/1.png');"></div>
+				<div class="b-block__title h3"><a href="not-found.php">Почтовый экспресс</a></div>
+				<div class="b-block__subtitle">0% — разумное решение.</div>
+				<div class="card__options">
+					<div class="card__option">
+						<div class="card__option-val">15 000 ₽</div>
+						<div class="card__option-title">
+							Кредитный лимит
+						</div>
+					</div>
+					<div class="card__option">
+						<div class="card__option-val">0,00%</div>
+						<div class="card__option-title">
+							Постоянная ставка
+						</div>
+					</div>
+				</div>
+				<div class="card__btn"><a href="anketa-card.php" class="btn">Оформить карту</a></div>
+			</div>
+			<div class="b-block cards__item">
+				<div class="cards__item-pattern"></div>
+				<div class="cards__item-img" style="background-image: url('/static/theme/images/cards/1.png');"></div>
+				<div class="b-block__title h3"><a href="not-found.php">Зеленый мир</a></div>
+				<div class="b-block__subtitle">Ваш вклад в озеленение планеты.</div>
+				<div class="card__options">
+					<div class="card__option">
+						<div class="card__option-val">500 000 ₽</div>
+						<div class="card__option-title">
+							Кредитный лимит
+						</div>
+					</div>
+					<div class="card__option">
+						<div class="card__option-val">1 дерево</div>
+						<div class="card__option-title">
+							За каждые 3 000 <span class="rub"></span>
+						</div>
+					</div>
+				</div>
+				<div class="card__btn"><a href="anketa-card.php" class="btn">Оформить карту</a></div>
+			</div>
+		</div>
+	</div>
+<?include "includes/footer.php"?>
+
