798048b8ec3002a9853bfc774868c8833d6bdf18
viktor <viktrgulakv@rambler.ru>
2017-06-29 19:05:13 +0300

    add:страница 404

--- static/not-found.php
+++ static/not-found.php
@@ -1,6 +1,33 @@
-<?include "includes/header.php"?>
-	<div class="grid__inner">
-		<h2>Страница не найдена</h2>
+<!DOCTYPE html>
+<html class="_not-found">
+<head>
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+	<meta charset="utf-8">
+	<title>Сайт</title>
+	<link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i,800,800i&amp;subset=cyrillic" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=PT+Sans:400,400i,700,700i&amp;subset=cyrillic,cyrillic-ext,latin-ext" rel="stylesheet">
+	<link rel="stylesheet" href="/static/theme/js/jquery-ui/jquery-ui.min.css">
+	<link rel="stylesheet" href="/static/theme/js/swiper/dist/css/swiper.min.css">
+	<link rel="stylesheet" href="/static/theme/js/fancybox/jquery.fancybox.css">
+	<link rel="stylesheet" href="/static/theme/js/chosen/chosen.css">
+	<link rel="stylesheet" href="/static/theme/css/styles.css">
+
+	<script src="/static/theme/js/libs.js"></script>
+	<script src="/static/theme/js/main.js"></script>
+</head>
+<body  class="">
+	<div class="grid" data-no-fixed-footer>
+		<div class="grid__inner">
+			<div class="not-found">
+				<div class="h3">Страница не найдена</div>
+				<div class="b-block__subtitle">Но мы уже её ищем!</div>
+				<div class="not-found__logo"></div>
+				<div class="not-found__back">
+					<a href="javascript:history.back();">Вернуться на сайт</a>
+				</div>
+			</div>
+		</div>
 	</div>
-<?include "includes/footer.php"?>
+</body>
+</html>
 
